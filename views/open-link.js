1d21efe21f8249e4a719e7602095cf76e3e4c678
jund-fauz <mujadidjundifauzi200@gmail.com>
2024-02-06 17:17:58 +0700

    Feat: Open document in new tab

--- views/open-link.js
+++ views/open-link.js
@@ -2,7 +2,7 @@ const linkButton = (link, id) =>
   `${
     link == ''
       ? `<button id="create-link-${id}" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#create-link-dialog">Masukkan Link</button>`
-      : `<a href="${link}" type="button" class="btn btn-primary">Buka</a>`
+      : `<a href="${link}" type="button" class="btn btn-primary" target="_blank">Buka</a>`
   }
   <button type="button" class="btn btn-primary ms-1" id="edit-${id}" hidden data-bs-toggle="modal" data-bs-target="#edit-dialog">
     <i class="fa-solid fa-pencil"></i>
